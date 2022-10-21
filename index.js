78113bc5074054cc2b32e66222e8a682f504dce4
rk14283 <rk14283@my.bristol.ac.uk>
2022-10-21 08:32:48 +0200

    Added GET, POST/boxers + express JSON middleware.

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+app.use(express.json())
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) =>
+const boxers = [
+{id:1, name:"Thomas Hearns"},
+{id: 2, name: "Roberto Duran"}, 
+{id: 3, name: "Sugar Ray Leonard"},
+{id: 4, name: "Marvelous Marvin Hagler"}
+]
+
+app.get("/boxers", (req,res) =>
 {
-    res.send("server working ")
+    res.json(boxers); 
 })
+
+
+app.get("/", (req,res) =>{
+    res.send("This is the night of champions")
+})
+app.post("/boxers", (req,res) => {
+    console.log("Are we getting a post request?", req.body); 
+    const newBoxers = {id: boxers.length+1, name:req.body.name};
+    boxers.push(newBoxers)
+    //201 means created
+    res.status(201).json({message:"New boxer added to the hall of fame"})
+}); 
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on ${PORT}`)
