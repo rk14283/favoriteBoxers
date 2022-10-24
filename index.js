692c837e4c41e93b4e52452fcf653a805a9b04fa
rk14283 <rk14283@my.bristol.ac.uk>
2022-10-24 19:44:30 +0200

    boxer id

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json())
+
+
+app.use(express.json()) //"use" ing middleware
 const PORT = process.env.PORT || 5000;
 
 const boxers = [
@@ -11,15 +13,30 @@ const boxers = [
 {id: 4, name: "Marvelous Marvin Hagler"}
 ]
 
+//GET /students 
 app.get("/boxers", (req,res) =>
 {
     res.json(boxers); 
 })
 
-
+GET /
 app.get("/", (req,res) =>{
     res.send("This is the night of champions")
 })
+
+//GET /boxers/:id
+//route parameter -> :id 
+app.get("/boxers/:id", (req,res)=>{
+    console.log("Hi", Number(req.params)); 
+    const boxer = boxers.find(
+        (boxer) => boxer.id === Number(req.params.id)
+    );
+    res.json(boxers); 
+}); 
+
+
+
+//POST Boxers 
 app.post("/boxers", (req,res) => {
     console.log("Are we getting a post request?", req.body); 
     const newBoxers = {id: boxers.length+1, name:req.body.name};
